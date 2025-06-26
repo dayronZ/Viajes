d28afc10f6099174419716ada90902b276d1d7aa
Droid636 <2022043@utsh.edu.mx>
2025-06-26 17:07:39 -0600

    secciones

--- vijes/src/main.jsx
+++ vijes/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+import Navbar from './components/inicio.jsx'
+import Seccion1 from './components/seccion1.jsx'
+import Seccion2 from './components/seccion2.jsx'
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Navbar/>
+    <Seccion1/>
+    <Seccion2/>
   </StrictMode>,
 )
