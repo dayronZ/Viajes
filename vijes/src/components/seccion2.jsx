26c8b5920560fce09a5fe305b80b38e662fdb394
Luisito <hernandezmercadoluisenrique@gmail.com>
2025-07-09 16:09:51 -0600

    [FIX] css correspondiente a galeria de imagenes

--- vijes/src/components/seccion2.jsx
+++
@@ -1,9 +0,0 @@
-const Seccion2 = () => {
-  return (
-    <section id="seccion2" className="h-screen flex items-center justify-center bg-pink-100">
-      <h2 className="text-3xl">Seccion 2</h2>
-    </section>
-  );
-};
-
-export default Seccion2;
--- vijes/src/main.jsx
+++ vijes/src/main.jsx
@@ -3,15 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 import Navbar from './components/inicio.jsx'
-import Seccion1 from './components/seccion1.jsx'
-import Seccion2 from './components/seccion2.jsx'
+import Seccion4 from './components/seccion4.jsx'
+import Seccion5 from './components/seccion5.jsx'
+
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Navbar/>
-    <Seccion1/>
-    <Seccion2/>
+    <Seccion4/>
+    <Seccion5/>
   </StrictMode>,
 )
