c9f56b56068c5af74ef95cdb308f1455651a103e
Droid636 <2022043@utsh.edu.mx>
2025-07-09 17:40:39 -0600

    Seccion proincipal terminada

--- vijes/src/components/seccion1.jsx
+++ vijes/src/components/seccion1.jsx
@@ -1,19 +1,19 @@
 const Seccion1 = () => {
   return (
     <section id="seccion1">
-      {/* Cargar CSS desde public */}
+    
       <link rel="stylesheet" href="/seccion1.css" />
 
-      {/* Video desde un enlace de internet */}
+      
       <video autoPlay muted loop playsInline>
-        <source src="https://cdn.pixabay.com/video/2024/03/12/203974-923133846_large.mp4" type="video/mp4" />
+        <source src="https://videos.pexels.com/video-files/3121327/3121327-uhd_2560_1440_24fps.mp4" type="video/mp4" />
         Tu navegador no soporta video HTML5.
       </video>
 
-      {/* Capa oscura encima del video */}
+      
       <div className="overlay"></div>
 
-      {/* Contenido centrado */}
+      
       <div className="content">
         <h1>Explora el mundo con nosotros</h1>
         <p>Viajes inolvidables, aventuras únicas y recuerdos para toda la vida.</p>
---
+++ vijes/src/components/seccion3.jsx
@@ -0,0 +1,9 @@
+const Seccion1 = () => {
+  return (
+    <section id="seccion3">
+   
+    </section>
+  );
+};
+
+export default Seccion1;
