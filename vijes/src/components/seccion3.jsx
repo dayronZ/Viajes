dabc4c4227b37688f7d2c8083fc91c092d02a45d
Droid636 <2022043@utsh.edu.mx>
2025-07-09 19:55:52 -0600

    Seccion 3

--- vijes/src/components/seccion3.jsx
+++ vijes/src/components/seccion3.jsx
@@ -1,9 +1,34 @@
-const Seccion1 = () => {
+import React from 'react';
+import './Seccion3.css';
+
+const Seccion3 = () => {
   return (
-    <section id="seccion3">
-   
+    <section id='seccion3' className="oferta-section">
+      <div className="oferta-content">
+        <div className="oferta-left">
+          <h2>Nuevas ofertas han llegado,<br />¡Aprovéchalas ahora mismo!</h2>
+          <p>
+            Descubre productos seleccionados con precios especiales por tiempo limitado. 
+            Disfruta de sabores únicos, ingredientes frescos y opciones para todos los gustos.
+          </p>
+          <p>
+            Perfecto para compartir en familia o sorprender a alguien especial. 
+            Ordena ahora y disfruta de lo mejor en la comodidad de tu hogar.
+          </p>
+          <div className="oferta-buttons">
+            <button className="btn-discover">DESCUBRIR</button>
+            <button className="btn-purchase">COMPRAR AHORA</button>
+          </div>
+        </div>
+
+        <div className="oferta-images">
+          <img src="/src/assets/italia2.jpg" alt="Comida 1" className="img img1" />
+          <img src="/src/assets/japon2.jpg" alt="Comida 2" className="img img2" />
+          <img src="/src/assets/mexico.jpg" alt="Comida 3" className="img img3" />
+        </div>
+      </div>
     </section>
   );
 };
 
-export default Seccion1;
+export default Seccion3;
--- vijes/src/main.jsx
+++ vijes/src/main.jsx
@@ -7,6 +7,8 @@ import Seccion1 from './components/seccion1.jsx'
 import Seccion2 from './components/seccion2.jsx'
 import Seccion6 from './components/Seccion6.jsx'
 import Seccion7 from './components/Seccion7.jsx'
+import Seccion3 from './components/seccion3.jsx'
+
 
 
 createRoot(document.getElementById('root')).render(
@@ -14,6 +16,7 @@ createRoot(document.getElementById('root')).render(
     <Navbar/>
     <Seccion1/>
     <Seccion2/>
+    <Seccion3/>
     <Seccion6/>
     <Seccion7/>
   </StrictMode>,
