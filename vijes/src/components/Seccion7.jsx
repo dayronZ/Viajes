77bf567733c9d9b42b424e5f72597659bc2d8bac
amillaly <leydif840@gmail.com>
2025-07-10 17:10:27 -0600

    Se creo el contenerdor como ejemplo de donde va a estar el mapa, se creo el formulario con los input de email y name, con su respectivo css

--- vijes/src/components/Seccion7.jsx
+++ vijes/src/components/Seccion7.jsx
@@ -4,9 +4,20 @@ import './Seccion7.css';
 const Seccion7 = () => {
   return (
     <section id="seccion7">
-     <div className="seccion7-content">
+      <h2 className="contact-title">CONTACT US</h2>
+      <div className="contact-container">
+        <div className="contact-card-empty">Aqui va el mapa</div>
 
-     </div>
+        <form className="contact-form">
+          <div className="contact-row">
+            <input type="text" placeholder="Your name..." className="contact-input" />
+            <input type="email" placeholder="Your email..." className="contact-input" />
+          </div>
+          <textarea placeholder="Your message..." className="contact-textarea"></textarea>
+          <button type="submit" className="contact-btn">SEND MESSAGE</button>
+        </form>
+        
+      </div>
     </section>
   );
 };
