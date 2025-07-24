65b00bb08681fc18ef9e14ee12b229aecbd01cf8
Droid636 <2022043@utsh.edu.mx>
2025-07-24 17:13:38 -0600

    Navegacion cotizacion

--- vijes/src/main.jsx
+++ vijes/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 
 import Navbar from './components/inicio.jsx';
@@ -11,6 +12,8 @@ import Seccion5 from './components/seccion5.jsx';
 import Seccion6 from './components/seccion6.jsx';
 import Seccion7 from './components/Seccion7.jsx';
 import Seccion8 from './components/Seccion8.jsx';
+import Seccion9 from './components/seccion9.jsx';
+
 
 const App = () => {
   return (
@@ -23,13 +26,22 @@ const App = () => {
       <Seccion5 />
       <Seccion6 />
       <Seccion7 />
-      <Seccion8/>
+      <Seccion8 />
+      <Seccion9 />
     </>
   );
 };
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-    </StrictMode>
+    <BrowserRouter>
+      <Routes>
+      
+        <Route path="/" element={<App />} />
+        
+        
+        <Route path="/cotizador" element={<Seccion9 />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 );
