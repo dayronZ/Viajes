f223dd28cb7a7299c918e2ce1d651dbed5718547
00-aprendiendo-react <dayron.delrosal@gmail.com>
2025-08-07 19:23:15 -0600

    Resolvimos conflictos

--- vijes/src/components/inicio.jsx
+++ vijes/src/components/inicio.jsx
@@ -78,7 +78,7 @@ const Navbar = () => {
         <ul className="menu">
           <li><a href="#seccion1">Inicio</a></li>
           <li><a href="#seccion2">Paquetes</a></li>
-          <li><a href="#seccion3">Todo Incluido</a></li>
+          <li><a href="#seccion3">Cotizar</a></li>
           <li><a href="#seccion4">Más visitados</a></li>
           <li><a href="#seccion5">Blogs</a></li>
           <li><a href="#seccion6">Costos</a></li>
